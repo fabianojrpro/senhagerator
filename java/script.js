d14b3454aed8540036e6fe565c6ff9430dbd54d9
Fabiano Junior <99301175+fabianojrpro@users.noreply.github.com>
2022-02-26 14:52:04 -0300

    Commit inicial
    Gerador de senha automático, com javascript.

---
+++ java/script.js
@@ -0,0 +1,31 @@
+let sliderElemente = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvxywzABCDEFGHIJKLMNOPQRSTUVXWYZ'!@#$%&*()-+={};:";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElemente.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElemente.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copypassword(){
+    alert("senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+}
